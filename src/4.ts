fffe2ebf6545114e25d1f34b6396adad6f785318
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-02-09 22:06:27 +0200

    new: 4-task

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,50 @@
-// const key = new Key();
+class Key {
+    private signature = Math.random();
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+    getSignature() {
+        return this.signature;
+    }
+}
 
-// house.openDoor(person.getKey());
+class Person {
+    constructor(private key: Key) {}
 
-// house.comeIn(person);
+    getKey() {
+        return this.key;
+    }
+}
 
-// export {};
+abstract class House {
+    constructor(key: Key) {}
+    door: true | false;
+
+    comeIn(person: Person) {
+        const tenants: Array<Person> = [];
+        if (this.door) {
+            tenants.push(person);
+            console.log(`${tenants.length} person is in the House`);
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key) {
+            this.door = true;
+            console.log("We have a key. The door is opening");
+        }
+    }
+}
+const key = new Key();
+
+const person = new Person(key);
+
+const house = new MyHouse(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
